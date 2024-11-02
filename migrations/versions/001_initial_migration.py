519a4b1a667bb69827c31bcf35404aa5cd622e8f
Vinicius Minatogawa <minatogawavlf@gmail.com>
2024-11-02 19:22:07 -0300

    Add initial database migration

---
+++ migrations/versions/001_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: 001
+Revises: 
+Create Date: 2024-11-02 19:20:41.141980
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '001'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
